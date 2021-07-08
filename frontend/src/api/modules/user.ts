8c460a87ee34501591807aea7b3d3c772cf1b790
xiaolang <facelanglang@gmail.com>
2021-07-08 16:16:04 +0800

    chore: change password

--- frontend/src/api/modules/user.ts
+++ frontend/src/api/modules/user.ts
@@ -3,5 +3,6 @@ export default {
   list: 'get /v1/user/list',
   signIn: 'post /v1/user/signIn',
   setStatus: 'post /v1/user/setStatus',
-  update: 'post /v1/user/update'
+  update: 'post /v1/user/update',
+  changePassword: 'post /v1/user/changePassword'
 }
--- frontend/src/views/user/UserEdit.vue
+++ frontend/src/views/user/UserEdit.vue
@@ -2,29 +2,51 @@
     <div>
         <el-row type="flex" justify="center">
             <el-card style="width:500px">
-                <el-form ref="form" :model="form" label-width="80px">
-                    <el-form-item label="用户名">
-                        <el-input></el-input>
+                <el-form ref="root" :model="form" label-width="80px">
+                    <el-form-item label="用户名" prop="name" :rules="simpleRule">
+                        <el-input v-model="form.name"></el-input>
                     </el-form-item>
-                    <el-form-item label="旧密码">
-                        <el-input></el-input>
+                    <el-form-item label="旧密码" prop="oldPassword" :rules="simpleRule">
+                        <el-input type="password" autocomplete="new-password" v-model="form.oldPassword"></el-input>
                     </el-form-item>
-                    <el-form-item label="密码">
-                        <el-input></el-input>
+                    <el-form-item label="密码" prop="newPassword" :rules="simpleRule">
+                        <el-input type="password" autocomplete="new-password" v-model="form.newPassword"></el-input>
                     </el-form-item>
                 </el-form>
                 <el-row type="flex" justify="center">
-                    <el-button type="primary" class="button">提交</el-button>
+                    <el-button @click="submit" type="primary" class="button">提交</el-button>
                 </el-row>
             </el-card>
         </el-row>
     </div>
 </template>
 <script>
+import { ref, toRefs, watch, reactive, onMounted } from 'vue'
+import $api from '@/api/index'
 export default {
-  date () {
+  setup () {
+    const root = ref(null)
+    const state = reactive({
+      form: {
+        name: '',
+        oldPassword: '',
+        newPassword: ''
+      },
+      simpleRule: { required: true, message: '必填项', trigger: 'change' }
+    })
+    const submit = () => {
+      root.value.validate(val => {
+        if (val) {
+          $api.user.changePassword({
+            ...state.form
+          }, { success: true })
+        }
+      })
+    }
     return {
-      form: {}
+      ...toRefs(state),
+      submit,
+      root
     }
   }
 }
